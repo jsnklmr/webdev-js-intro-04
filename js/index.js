18bdfd7df5c84598c69f996d3f0a90d99335697d
Jason Kilmer <jkilmer@thedixongroup.com>
2025-01-23 11:53:17 -0500

    tasks part 1 complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+let healthPoints = 5;
+
+if (healthPoints >= 10) {
+    console.log("Hero is healthy.");
+} else if (healthPoints < 10 && healthPoints >= 5) {
+    console.log("Hero is slightly damaged.");
+} else {
+    console.log("Hero is close to death.");
+}
